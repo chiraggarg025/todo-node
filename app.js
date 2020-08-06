ec0a0788d81f2e32ecadcd1a168adf5945a7cc7d
chanakya.garg <chiraggarg025@gmail.com>
2020-08-06 21:25:43 +0530

    install express

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express  = require('express');
 const app = express();
+const port = 8000;
+app.get('/',function(req,res){
+    res.end('<h1>HI</h1>');
+})
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
