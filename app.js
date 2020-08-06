c5eea1163f2ac2c103d5875e4db59967bf5b0dbd
chanakya.garg <chiraggarg025@gmail.com>
2020-08-06 22:02:23 +0530

    install ejs add home page

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express  = require('express');
+// requiring express
 const app = express();
 const port = 8000;
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+// setting location for assets
+app.use(express.static('./assets'));
+// getting the homepage
 app.get('/',function(req,res){
-    res.end('<h1>HI</h1>');
+    res.render('home');
 })
-
-
+// listening to port
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
