aaff49bfc3e22234452939c77716f2cd040cdf6e
chanakya.garg <chiraggarg025@gmail.com>
2020-08-09 23:04:11 +0530

    make searchbar functional

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,19 @@ for(let i=0;i<checked.length;i++){
     checked[i].style.color = "white";
     checked[i].style.textDecoration = "line-through";
 }
+document.getElementById('searchbar').addEventListener('keyup',search_habit);
+// function to make searchbar work
+function search_habit() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('tasks-form'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="block";                  
+        } 
+    } 
+} 
